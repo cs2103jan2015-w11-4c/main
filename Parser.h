7ca59e8854cf4f22ad1aa1b852fd775c24cbac45
XieXinxx <peter_xiexin@163.com>
2015-03-04 11:18:18 +0800

    Parser Skeleton(not finished)
    Headers, definitions, assignments

---
+++ Parser.h
@@ -0,0 +1,35 @@
+#ifndef COMMANDPARSER_H
+#define COMMANDPARSER_H
+
+#include "Task.h"
+#include "Calendar.h"
+#include "Utility.h"
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class CommandParser {
+public:
+	static const string NOT_EXIST;
+
+	static string getCommand(string input);
+	static string getStartYear();
+	static string getStartMonth();
+	static string getStartDay();
+	static string getStartHour();
+	static string getStartMinute();
+	static string getEndYear(string input);
+	static string getEndMonth(string input);
+	static string getEndDay(string input);
+	static string getEndHour(string input);
+	static string getEndMinute(string input);
+	static string getPriority(string input);
+	static string getName(string input);
+	static string getDescription(string input);
+	static string getLocation(string input);
+	static string getKeywords(string input);
+	static vector<string> getParsedUserInput(string uInput);
+
+//header ends
