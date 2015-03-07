a757de3fa91a051e95d00b437c938654d167a009
gabrielhon <gabrielhon@gmail.com>
2015-03-07 20:47:11 +0800

    added UI.h

---
+++ Jarvis/Jarvis/UI.h
@@ -0,0 +1,20 @@
+#ifndef UI_H
+#define UI_H
+
+#include <string>
+using namespace std;
+
+class Task {
+
+private:
+	string _read;
+	string _diaply;
+	const string added, deleted, edited;
+
+public:
+	void readLine(string);
+	void getStatus(bool status);
+	void display();
+}
+
+#endif
