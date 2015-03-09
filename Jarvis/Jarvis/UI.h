5c002f620a3d27b39bf45b7886d898740994c8b4
gabrielhon <gabrielhon@gmail.com>
2015-03-09 15:51:41 +0800

    Added UI.cpp

--- Jarvis/Jarvis/UI.h
+++ Jarvis/Jarvis/UI.h
@@ -4,12 +4,16 @@
 #include <string>
 using namespace std;
 
-class Task {
+class UI {
 
 private:
 	string _read;
 	string _display;
-	const string added, deleted, edited;
+	const string ADDED = "Task has been added\n";
+	const string DELETED = "Task has been deleted\n";
+	const string EDITED = "Task has been edited\n";
+	const string WELCOME = "Welcome!";
+
 
 public:
 	void readLine(string);
