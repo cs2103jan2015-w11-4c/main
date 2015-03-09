6ab26fe912b3cd5effda6cd5c56b0013cd2a6061
toothfairy94 <zannwky@gmail.com>
2015-03-10 04:52:18 +0800

    Revert "Changed storage"
    This reverts commit 2f7ce1213c19b5656251bfb64768bf09f9c03b3f.

--- Jarvis/Jarvis/Storage.h
+++ Jarvis/Jarvis/Storage.h
@@ -17,7 +17,7 @@ private:
 
 
 public:
-	bool writeFile(string, string);   //for adding/editing return boolean to Logic
+	bool writeFile(string, vector <string> tasklist);   //for adding/editing return boolean to Logic
 	vector <string> readFile (string);		//display items
 	//string deleteTask(string);
 	//bool returnStatus();        //if adding/delete succeed return with boolean 0 or 1
