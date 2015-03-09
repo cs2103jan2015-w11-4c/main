6eece4b22d2c7526eabba7ba87768498ff49620b
gabrielhon <gabrielhon@gmail.com>
2015-03-09 16:26:01 +0800

    Edited UI.cpp again

--- Jarvis/Jarvis/UI.cpp
+++ Jarvis/Jarvis/UI.cpp
@@ -4,7 +4,35 @@
 
 using namespace std;
 
-void UI::readLine(string UserInput)
-{
+const string UI::MESSAGE_COMMAND = "command: ";
+const string UI::MESSAGE_ADDED = "Task has been added.";
+const string UI::MESSAGE_DELETED = "Task has been deleted.";
+const string UI::MESSAGE_EDITED = "Task has been edited.";
+const string UI::MESSAGE_WELCOME = "Welcome to Jarvis. %s is ready.";
+
+bool isRunning = true;
+
+int main(int argc, char* argv[]){
+	UI::main(argc, argv);
+	return 0;
+}
+
+void UI::main(int argc, char* argv[]){
+	string fileName = argv[1];
+	sprintf_s(buffer, MESSAGE_WELCOME.c_str(), argv[1]);
+	display(buffer);
+
+	while (isRunning){
+		cout << MESSAGE_COMMAND;
+		string userInput;
+		//readLine(UserInput)
+	}
+
+}
+void UI::display(string text){
+	cout << text << endl;
+}
+
+void UI::readLine(string UserInput){
 	return;
 }
