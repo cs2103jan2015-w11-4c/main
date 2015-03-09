a13f38803df17b6669c53ae7c4aaf84c3595ff1c
gabrielhon <gabrielhon@gmail.com>
2015-03-09 16:20:58 +0800

    Edited UI.cpp
    messages added

--- Jarvis/Jarvis/UI.h
+++ Jarvis/Jarvis/UI.h
@@ -9,16 +9,21 @@ class UI {
 private:
 	string _read;
 	string _display;
-	const string ADDED = "Task has been added\n";
-	const string DELETED = "Task has been deleted\n";
-	const string EDITED = "Task has been edited\n";
-	const string WELCOME = "Welcome!";
+	static const int MAX_BUFFER_SIZE = 255;
+	static char buffer[MAX_BUFFER_SIZE];
+
+	static const string MESSAGE_COMMAND;
+	static const string MESSAGE_ADDED;
+	static const string MESSAGE_DELETED;
+	static const string MESSAGE_EDITED;
+	static const string MESSAGE_WELCOME;
 
 
 public:
+	static void main(int , char* []);
 	void readLine(string);
 	void getStatus(bool status);
-	void display();
+	static void display(string);
 }
 
 #endif
