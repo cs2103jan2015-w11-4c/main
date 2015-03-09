bc779967614cf35390419ea95706b86b338057ae
XieXinxx <peter_xiexin@163.com>
2015-03-09 16:36:28 +0800

    updates

--- parser.cpp
+++ parser.cpp
@@ -90,51 +90,6 @@ vector<string> CommandParser::getParsedUserInput(string input){
 	vector<string> parsedInput(Task::ATTR::SIZE);
 	parsedInput[Task::ATTR::COMMAND] = getCommand(input);
 
-	switch (cmd) {
-	case Calendar::COMMAND_TYPE::TASKADD:
-		startDate = getStartDate(input);
-		startTime = getStartTime(input);
-		location = getLocation(input);
-		name = getName(input);
-		endDate = getEndDate(input);
-		parsedInput[Task::ATTR::STARTYEAR] = getStartYear();
-		parsedInput[Task::ATTR::STARTMONTH] = getStartMonth();
-		parsedInput[Task::ATTR::STARTDAY] = getStartDay();
-		parsedInput[Task::ATTR::STARTHOUR] = getStartHour();
-		parsedInput[Task::ATTR::STARTMINUTE] = getStartMinute();
-		parsedInput[Task::ATTR::ENDYEAR] = getEndYear(input);
-		parsedInput[Task::ATTR::ENDMONTH] = getEndMonth(input);
-		parsedInput[Task::ATTR::ENDDAY] = getEndDay(input);
-		parsedInput[Task::ATTR::ENDHOUR] = getEndHour(input);
-		parsedInput[Task::ATTR::ENDMINUTE] = getEndMinute(input);
-		parsedInput[Task::ATTR::NAME] = getName(input);
-		parsedInput[Task::ATTR::DESCRIPTION] = getDescription(input);
-		parsedInput[Task::ATTR::ISDONE] = "false";
-		if (startDate == CommandParser::NOT_EXIST && endDate == CommandParser::NOT_EXIST) {
-			parsedInput[Task::ATTR::HASDATE] = CommandParser::NOT_EXIST;
-			parsedInput[Task::ATTR::HASEXPIRED] = CommandParser::NOT_EXIST;
-		}
-		else{
-			parsedInput[Task::ATTR::HASDATE] = "true";
-			parsedInput[Task::ATTR::HASEXPIRED] = "false";
-		}
-		break;
-	case Calendar::COMMAND_TYPE::TASKEDIT:
-		parsedInput.clear();
-		parsedInput.resize(EDIT::LASTPOSITION);
-		parsedInput[EDIT::SEARCH_KEYWORDS] = getKeywords(input);
-		break;
-	case Calendar::COMMAND_TYPE::TASKREMIND:
-		parsedInput[REMIND::SEARCH_KEYWORDS] = getKeywords(input);
-		break;
-	case Calendar::COMMAND_TYPE::TASKREPEAT:
-		parsedInput[REPEAT::SEARCH_KEYWORDS] = getKeywords(input);
-		break;
-	default:
-		parsedInput[Task::ATTR::NAME] = getKeywords(input);
-		break;
-	}
-
 	return parsedInput;
 }
 
