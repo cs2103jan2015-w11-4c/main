fd51a50d8370f0e1b66f275dfda5385bad9d6141
XieXinxx <peter_xiexin@163.com>
2015-03-10 17:48:40 +0800

    updated parser

--- Jarvis/Jarvis/parser.cpp
+++ Jarvis/Jarvis/parser.cpp
@@ -4,82 +4,26 @@
 
 using namespace std;
 
-
+const string CommandParser::UpdateIndicator = " to ";
 const string CommandParser::commandIndicator = " ";
-const string CommandParser::startDateIndicator = " on ";
-const string CommandParser::startDayIndicator = "/";
-const string CommandParser::startTimeIndicator = " at ";
-const string CommandParser::locationIndicatorStart = " <";
-const string CommandParser::locationIndicatorEnd = "> ";
-const string CommandParser::currentYear = "2014";
-const string CommandParser::hourIndicator = ":.";
-const string CommandParser::endDateIndicator = " by ";
-const string CommandParser::endDayIndicator = "/";
 
-const int CommandParser::startingPosition = 0;
+
 const int CommandParser::positionModerator1 = 1;
-const int CommandParser::positionModerator2 = 2;
-const int CommandParser::positionModerator3 = 3;
-const int CommandParser::positionModerator4 = 4;
 
 string CommandParser::userInput = "";
-string CommandParser::startDate = "";
-string CommandParser::startTime = "";
-string CommandParser::endDate = "";
-string CommandParser::location = "";
-string CommandParser::name="";
-vector<string> CommandParser::parsedInput;
 
-CommandParser::userInput = ui; 
+//vector<string> CommandParser::parsedInput;
 
-string CommandParser::getCommand(string input){
-	string cmd = input.substr(startingPosition, input.find(commandIndicator));
-	Calendar::COMMAND_TYPE command = Utility::stringToCOMMAND_TYPE(cmd);
-	CommandParser::_command = command;
+//CommandParser::userInput = ui; 
 
-	if (Utility::isValidCommand(command)) { //if the command is a valid command type, return it. else return "add"
-		return cmd;
-	}
-}
-string CommandParser::getStartDate(string input) {
+string CommandParser::getUpdate(string input){
 	try {
-		unsigned int start = input.rfind(startDateIndicator);
-		if (start == string::npos) {
-			return CommandParser::NOT_EXIST;
-		}
-		start += positionModerator4;
-		startDate = input.substr(start, input.size() - start);
-		return startDate;
-	} catch (exception &) {
-		return CommandParser::NOT_EXIST;
-	}
-}
-string CommandParser::getEndDate(string input){
-	try {
-		unsigned int start = input.rfind(endDateIndicator);
-		if (start == string::npos) {
-			return CommandParser::NOT_EXIST;
-		}
-		start += positionModerator4;
-		endDate = input.substr(start, input.size() - start);
-		return endDate;
-	} catch (exception &) {
-		return CommandParser::NOT_EXIST;
-	}
-}
-
-string CommandParser::getKeywords(string input) {
-
-	try {
-		unsigned int start = userInput.find_first_of(commandIndicator);
+		unsigned int start = userInput.find_first_of(UpdateIndicator);
 		start += positionModerator1;
-		return input.substr(start, input.size()-start);
-	} catch (exception &) {
+		return input.substr(start input.size()-start);
+		} catch (exception &) {
 		return CommandParser::NOT_EXIST;
 	}
-}
-string CommandParser::getDescription(string input) {
-	return CommandParser::NOT_EXIST;
 }
 
 
@@ -89,10 +33,10 @@ string CommandParser::getDescription(string input) {
 
 vector<string> CommandParser::getParsedUserInput(string input){
 
-	vector<string> CommandParser::parsedInput(Task::ATTR::SIZE);
-	CommandParser::parsedInput[Task::ATTR::COMMAND] = CommandParser::getCommand(input);
+	//vector<string> CommandParser::parsedInput(Task::ATTR::SIZE);
+	//CommandParser::parsedInput[Task::ATTR::COMMAND] = CommandParser::getCommand(input);
 
-	return CommandParser::parsedInput;
+	return CommandParser::ParsedUserInput;
 }
 
 #endif
