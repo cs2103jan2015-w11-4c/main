4cad59550f6ab2cb750de564a09fd287fd10dad8
gabrielhon <gabrielhon@gmail.com>
2015-03-10 18:09:20 +0800

    UI changes look

--- Jarvis/Jarvis/UI.h
+++ Jarvis/Jarvis/UI.h
@@ -9,18 +9,17 @@ class UI {
 
 private:
 	static const int MAX_BUFFER_SIZE = 255;
-	static char buffer[MAX_BUFFER_SIZE];	
+	static char buffer[MAX_BUFFER_SIZE];
 	static const string MESSAGE_COMMAND;
 	static const string MESSAGE_WELCOME;
-
+	static const string MESSAGE_BYE;
 
 public:
 
 	static void main(int, char*[]);
-	//static void readLine(string);
 	static void displayLine(string);
 	void getStatus(bool status);
-	
+
 };
 
 #endif
