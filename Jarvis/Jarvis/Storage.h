90b64ee35683fdd420dd4f1de7111889948c0516
toothfairy94 <zannwky@gmail.com>
2015-04-13 01:10:55 +0800

    Refactored Storage!! (:

--- Jarvis/Jarvis/Storage.h
+++ Jarvis/Jarvis/Storage.h
@@ -19,13 +19,19 @@ private:
 public:
 
 	bool writeFile(Task, string,string);   //for adding/editing return boolean to Logic
+	bool checkAdd(Task, string,string);
 	string readFile (string,string);		//display items
 	bool clearFile(string,string);
+	bool checkClear(string,string);
 	bool replaceFileData(string,string,string);
+	void replaceData (string,string,string,string);
+	bool checkDelete(string,string,string);
 	int startIndex(string);
 	int endIndex(string);
 	string extractUserCommand(string,string&);
 	bool updateFileData(string,string,string,string);
+	void replaceUpdateData(string, string,string,string, string);
+	bool checkUpdate(string, string,string,string);
 	vector <string> getTaskVector();
 	bool writeBackToFile(string,string,string);
 	string lowerCase(string);
